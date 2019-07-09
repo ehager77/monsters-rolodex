2fa0a0e9c4221bcded70b2cc9cbdb8d54287cc60
ehager77 <eric@isabel.io>
2019-07-09 16:01:35 -0700

    Updates

---
+++ precache-manifest.f691775f9bad60da525a7f80e5333494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a58b198c339d964f32f950bb79debb5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d2f0671dd38c4e8d4add",
+    "url": "/monsters-rolodex/static/css/main.735d6123.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "d2f0671dd38c4e8d4add",
+    "url": "/monsters-rolodex/static/js/main.d5504ca6.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
